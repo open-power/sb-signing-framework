94d1d4be316a414b1e5c64f6ff30c55b7c36538a
Nick Child <nick.child@ibm.com>
2021-03-09 16:11:55 -0500

    sign_cert_test.sh: Add correct license

--- src/client/sign_cert_test.sh
+++ src/client/sign_cert_test.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#			sign_cert Regression Test
-#			     Written by Ken Goldman, Ported by Nick Child (2021)
-#		       IBM Thomas J. Watson Research Center
-#	      $Id: regression.sh 433 2014-03-05 21:58:40Z kgoldman $
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
-#			       IBM Confidential
-#			     OCO Source Materials
-#			 (c) Copyright IBM Corp. 2013
-#
-#	   The source code for this program is not published or otherwise
-#	   divested of its trade secrets, irrespective of what has been
-#	   deposited with the U.S. Copyright Office
-#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 # Run this script from client side
 
